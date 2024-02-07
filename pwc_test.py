a9523982c109fa63d34e22b021e7ddf0f5ca1714
CuberHuber <lupashcko.rom@yandex.ru>
2024-02-07 17:36:44 +0300

    dump

---
+++ pwc_test.py
@@ -0,0 +1,89 @@
+"""
+Нагрузка плагина SPP
+
+1/2 документ плагина
+"""
+import logging
+import pickle
+
+from src.spp.types import SPP_document
+
+
+class PriceWaterhouseCooprs:
+    """
+    Класс парсера плагина SPP
+
+    :warning Все необходимое для работы парсера должно находится внутри этого класса
+
+    :_content_document: Это список объектов документа. При старте класса этот список должен обнулиться,
+                        а затем по мере обработки источника - заполняться.
+
+
+    """
+
+    SOURCE_NAME = 'pwc'
+    _content_document: list[SPP_document]
+
+    HOST = 'https://www.pwc.com'
+
+    def __init__(self, dump, *args, **kwargs):
+        """
+        Конструктор класса парсера
+
+        По умолчанию внего ничего не передается, но если требуется (например: driver селениума), то нужно будет
+        заполнить конфигурацию
+        """
+        # Обнуление списка
+        self._content_document = []
+
+        self.dump = dump
+
+        # Логер должен подключаться так. Вся настройка лежит на платформе
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.debug(f"Parser class init completed")
+        self.logger.info(f"Set source: {self.SOURCE_NAME}")
+        ...
+
+    def content(self) -> list[SPP_document]:
+        """
+        Главный метод парсера. Его будет вызывать платформа. Он вызывает метод _parse и возвращает список документов
+        :return:
+        :rtype:
+        """
+        self.logger.debug("Parse process start")
+        self._parse()
+        self.logger.debug("Parse process finished")
+        return self._content_document
+
+    def _parse(self):
+        """
+        Метод, занимающийся парсингом. Он добавляет в _content_document документы, которые получилось обработать
+        :return:
+        :rtype:
+        """
+        # HOST - это главная ссылка на источник, по которому будет "бегать" парсер
+        self.logger.debug(F"Parser enter to {self.HOST}")
+
+        # ========================================
+        # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
+        # -
+        try:
+            docs = pickle.load(self.dump)
+            for doc in docs:
+                doc.abstract = doc.abstract.replace(' ', "'")
+                self._content_document.append(doc)
+        except Exception as e:
+            print(e)
+        # ---
+        # ========================================
+
+    @staticmethod
+    def _find_document_text_for_logger(doc: SPP_document):
+        """
+        Единый для всех парсеров метод, который подготовит на основе SPP_document строку для логера
+        :param doc: Документ, полученный парсером во время своей работы
+        :type doc:
+        :return: Строка для логера на основе документа
+        :rtype:
+        """
+        return f"Find document | name: {doc.title} | link to web: {doc.web_link} | publication date: {doc.pub_date}"
